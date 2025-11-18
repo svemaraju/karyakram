efcba41f022bb4315c03b6525a53b351f0470e2d
svemaraju <srikanth.vemaraju@gmail.com>
2025-11-18 23:44:46 +0000

    Add user model, generate migrations.

--- src/karyakram/user/models.py
+++ src/karyakram/user/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# We will create a custom user class,
+# and point django settings to this.
+# This will help us in adding any
+# custom attributes in the future.
+class User(AbstractUser):
+    pass
